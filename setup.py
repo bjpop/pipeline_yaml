9fbc8adab1f4fda86f9e1a743dac4d76764a52b8
Bernie Pope <bjpope@unimelb.edu.au>
2017-01-19 21:02:42 +1100

    Clean up repo

--- setup.py
+++ setup.py
@@ -3,12 +3,7 @@
 from distutils.core import setup
 
 LONG_DESCRIPTION = \
-'''The program reads one or more input FASTA files. 
-For each file it computes a variety of statistics, and then
-prints a summary of the statistics as output.
-        
-The goal is to provide a solid foundation for new bioinformatics command line tools,
-and is an ideal starting place for new projects.'''
+'''The program renders a pipeline diagram from a YAML file'''
 
 
 setup(
@@ -25,5 +20,5 @@ setup(
     license='LICENSE',
     description=('A prototypical bioinformatics command line tool'),
     long_description=(LONG_DESCRIPTION),
-    install_requires=["biopython==1.66"],
+    install_requires=["yaml"],
 )
